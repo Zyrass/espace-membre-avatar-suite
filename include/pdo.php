a09720e526420ceae35f202978bc228990174204
Alain Guillon <zyrass@outlook.fr>
2017-03-20 16:50:52 +0100

    Update pdo.php
    Ajout du code d'erreur de PDO

--- include/pdo.php
+++ include/pdo.php
@@ -1,6 +1,8 @@
 <?php
 try {
     $bdd = new PDO ('mysql:host=localhost; dbname=tuto', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->exec("SET NAMES utf8");
 } catch (Exception $e) {
     die("Error : " . $e->getMessage());
 }
